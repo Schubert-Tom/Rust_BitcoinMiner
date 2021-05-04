bebde2353a9760bc1fb51e6306566e83f9d521bf
suba14 <mot.schubert@gmail.com>
2021-05-04 22:38:10 +0200

    API

--- rustMiner/src/main.rs
+++ rustMiner/src/main.rs
@@ -1,22 +1,5 @@
-
-fn main() -> web3::Result<()> {
-    let transport = web3::transports::Http::new("http://localhost:8545")?;
-    let web3 = web3::Web3::new(transport);
-
-    println!("Calling accounts.");
-    let mut accounts = acc;
-    println!("Accounts: {:?}", accounts);
-    accounts.push("00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap());
-
-    println!("Calling balance.");
-    for account in accounts {
-       // let balance = web3.eth().balance(account, None).await?;
-        println!("Balance of {:?}: {}", account, balance);
-    }
-
-    Ok(())
+fn main(){
+    println!("HEllo Wolrd");
+    let transport = web3::api::eth::new("http://localhost:8545")?;
+    println!("HEllo Wolrd");
 }
-
-fn acc() -> web3::Result<()> {
-    let mut accounts = web3.eth().accounts().await?;
-} //Ich geh essen bye c ya l8r
---
+++ rustMiner/target/debug/build/tokio-0f839feea77dd886/build_script_build-0f839feea77dd886.d
@@ -0,0 +1,5 @@
+/Users/tom/Desktop/Rust/rustMiner/target/debug/build/tokio-0f839feea77dd886/build_script_build-0f839feea77dd886: /Users/tom/.cargo/registry/src/github.com-1ecc6299db9ec823/tokio-1.5.0/build.rs
+
+/Users/tom/Desktop/Rust/rustMiner/target/debug/build/tokio-0f839feea77dd886/build_script_build-0f839feea77dd886.d: /Users/tom/.cargo/registry/src/github.com-1ecc6299db9ec823/tokio-1.5.0/build.rs
+
+/Users/tom/.cargo/registry/src/github.com-1ecc6299db9ec823/tokio-1.5.0/build.rs:
